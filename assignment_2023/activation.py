b4eea22c326f42e1d5147b0322720ddcb671b105
mitukou1109 <mitukou1109@gmail.com>
2023-11-06 10:39:19 +0900

    sigmoidのオーバーフローを対策

--- assignment_2023/activation.py
+++ assignment_2023/activation.py
@@ -6,10 +6,6 @@ from .tensor import Tensor
 class Sigmoid:
     def __call__(self, input: Tensor) -> Tensor:
         return Tensor(
-            np.where(
-                input > 0,
-                1 / (1 + np.exp(-input)),
-                np.exp(input) / (1 + np.exp(input)),
-            ),
+            np.exp(np.minimum(input, 0)) / (1 + np.exp(-np.abs(input))),
             input.prev,
         )
