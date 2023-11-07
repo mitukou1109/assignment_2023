a6fe92746206b7c85f39f2a0bcac46dccec5a304
mitukou1109 <mitukou1109@gmail.com>
2023-11-07 11:38:06 +0900

    単一ニューロンを使うと遅かった

--- assignment_2023/linear.py
+++ assignment_2023/linear.py
@@ -1,20 +1,11 @@
 import numpy as np
 
-from .single_neuron import SingleNeuron
 from .tensor import Tensor
 
 
 class Linear:
     def __init__(self, in_features: int, out_features: int) -> None:
-        self.out_features = out_features
-        self.neurons = [SingleNeuron() for _ in range(out_features)]
         self.w = np.random.rand(out_features, in_features + 1) * 2 - 1
 
     def __call__(self, x: Tensor) -> Tensor:
-        y = Tensor(np.zeros((x.shape[0], self.out_features)), x)
-
-        for i in range(self.out_features):
-            self.neurons[i].w = self.w[i]
-            y[:, i] = self.neurons[i](x)
-
-        return y
+        return Tensor(np.hstack([x, np.ones((x.shape[0], 1))]) @ self.w.T, x)
--- assignment_2023/single_neuron.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-
-
-class SingleNeuron:
-    def __init__(self, in_features: int = -1) -> None:
-        self.w = np.random.rand(in_features + 1) * 2 - 1
-
-    def __call__(self, x: np.ndarray) -> np.ndarray:
-        return np.hstack([x, np.ones((x.shape[0], 1))]) @ self.w.T
