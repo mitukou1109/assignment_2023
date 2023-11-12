60155546c71afdc9235862b94c67af3a3b420878
mitukou1109 <mitukou1109@gmail.com>
2023-11-12 11:47:01 +0900

    タイトルを削除・余白を調整

--- assignment_2023/visualizer.py
+++ assignment_2023/visualizer.py
@@ -5,8 +5,6 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 csv_path = max(glob.glob("log/*.csv")) if len(sys.argv) < 2 else sys.argv[1]
-with open(csv_path) as f:
-    title = f.readline().lstrip("# ")
 result = np.loadtxt(csv_path, delimiter=",")
 
 epochs = result[:, 0]
@@ -26,8 +24,8 @@ else:
     )
 
 loss_curve = plt.figure(num="Loss")
+loss_curve.subplots_adjust(left=0.0875, right=0.975, bottom=0.1, top=0.975)
 ax = loss_curve.add_subplot()
-ax.set_title(title, wrap=True)
 ax.plot(epochs, train_loss)
 ax.set_xlabel("epoch")
 ax.set_ylabel("loss")
@@ -36,8 +34,8 @@ ax.set_xlim(1, epochs[-1])
 ax.grid(axis="y")
 
 learning_curve = plt.figure(num="Learning curve")
+learning_curve.subplots_adjust(left=0.1, right=0.975, bottom=0.1, top=0.975)
 ax = learning_curve.add_subplot()
-ax.set_title(title, wrap=True)
 ax.plot(epochs, train_acc, label="train")
 ax.plot(epochs, test_acc, label="test")
 ax.legend(loc="lower right")
